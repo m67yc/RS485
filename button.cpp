37788b0e78364b22eb15906ff54fbabb2fe429ff
m67yc <martinmcymay@gmail.com>
2024-11-13 16:59:29 +0800

    Create button.cpp

---
+++ button.cpp
@@ -0,0 +1,48 @@
+#include <SoftwareSerial.h>
+// #define DE 3
+// #define RE 2
+
+SoftwareSerial mySerial(2, 3);
+int buttonPin = 13;
+// int buttonPin1 = 4;
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  pinMode(buttonPin, INPUT_PULLUP);
+  // pinMode(buttonPin1, INPUT_PULLUP);
+  // pinMode(DE, OUTPUT);
+  // pinMode(RE, OUTPUT);
+  // digitalWrite(DE, HIGH);
+  // digitalWrite(RE, HIGH);
+}
+
+void loop() {
+
+  char a;
+  int error = 0;
+
+  if (digitalRead(buttonPin) == LOW) {
+    mySerial.write('A');
+    a = 'A';
+
+  } else {
+    mySerial.write('B');
+    a = 'B';
+  }
+
+  // if (digitalRead(buttonPin1) == LOW) {
+  //   mySerial.write('C');
+  // } else {
+  //   mySerial.write('D');
+  // }
+  while(mySerial.available() > 0){
+    char c = mySerial.read();
+    if(c != a){
+      Serial.println("error!");
+      Serial.print("  ;  ");
+    }
+    Serial.print(c);
+  }
+  delay(100);
+}
