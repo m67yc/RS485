5eccecbd5548b6ef06e08c04c32d5eba69e5bbc5
m67yc <martinmcymay@gmail.com>
2024-11-20 17:40:59 +0800

    Update RX_V2.cpp

--- RS485/RX_V2.cpp
+++ RS485/RX_V2.cpp
@@ -1,7 +1,6 @@
 #include <SoftwareSerial.h>
 #define ledPin 12
 SoftwareSerial RS485(2, 3);
-bool error;
 
 void setup() {
   Serial.begin(9600);
@@ -12,12 +11,11 @@ void setup() {
 void loop() {
   while (RS485.available() > 0) {
     char receivedVal = RS485.read();
-    long int counter = millis();
     RS485.write(receivedVal);
     Serial.println(receivedVal);
     if (receivedVal == 'L')
       digitalWrite(ledPin, HIGH);
-    else
+    else if(receivedVal == 'D')
       digitalWrite(ledPin, LOW);
   }
 }
