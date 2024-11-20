5fd9388d421c45de8a9fb426962c23cdf97d3d4f
m67yc <martinmcymay@gmail.com>
2024-11-20 14:58:32 +0800

    Update transmitter.cpp

--- transmitter.cpp
+++ transmitter.cpp
@@ -2,20 +2,24 @@
 
 #define buttonPin 13
 SoftwareSerial RS485(2, 3);
-bool error;
 long int delaySec = 350;
+
 void setup() {
+
   Serial.begin(9600);
   RS485.begin(9600);
   pinMode(buttonPin, INPUT_PULLUP);
+
 }
 
 void loop() {
+
   long int success_count;
   long int error_count;
-  int success_rate = error_count/(success_count+error_count)*100;
+  int success_rate = error_count / (success_count + error_count) * 100;
   char returnVal;
   char inputVal;
+
   if (digitalRead(buttonPin) == 0) {
     inputVal = 'L';
     RS485.write(inputVal);
@@ -23,16 +27,18 @@ void loop() {
     inputVal = 'D';
     RS485.write(inputVal);
   }
+
   returnVal = RS485.read();
   Serial.print(returnVal);
   Serial.print(" : ");
   Serial.println(inputVal);
+
   long int timer = millis();
-  if(timer - millis() >= delaySec){
-  if (returnVal != inputVal){
-     Serial.println("Error detected!");
-     error_count++;
-  }else
-    success_count++;
+  if (timer - millis() >= delaySec) {
+    if (returnVal != inputVal) {
+      Serial.println("Error detected!");
+      error_count++;
+    } else
+        success_count++; 
   }
 }
