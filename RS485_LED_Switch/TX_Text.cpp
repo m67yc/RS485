df343023d3f84851cbe6e428ab99104c360905ee
m67yc <martinmcymay@gmail.com>
2024-11-25 16:03:56 +0800

    Update TX_Text.cpp

--- RS485_LED_Switch/TX_Text.cpp
+++ RS485_LED_Switch/TX_Text.cpp
@@ -4,21 +4,17 @@ SoftwareSerial RS485(2, 3);
 
 String message = "";
 
-void setup(){
+void setup() {
 
   Serial.begin(9600);
   RS485.begin(9600);
   pinMode(buttonPin, INPUT_PULLUP);
-
 }
 
 void loop() {
 
   while (Serial.available()) {
-
     char received = Serial.read();
     RS485.write(received);
-
   }
-
 }
