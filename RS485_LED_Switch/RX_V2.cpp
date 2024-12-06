cf5dabdb76b3a74b5dd91dfba4c9dbabe05bd2a2
m67yc <martinmcymay@gmail.com>
2024-12-06 15:11:37 +0800

    Update and rename RX_V2_Text.cpp to RX_V2.cpp

--- RS485_LED_Switch/RX_V2.cpp
+++ RS485_LED_Switch/RX_V2.cpp
@@ -1,6 +1,4 @@
-#include <SoftwareSerial.h>
-#define ledPin 12
-SoftwareSerial RS485(2, 3);
+#define ledPin 13
 
 bool read = 0;
 String message = "";
@@ -9,27 +7,26 @@ char m[100];
 void setup() {
 
   Serial.begin(9600);
-  RS485.begin(9600);
+  Serial3.begin(9600);
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
 
-  int time = 0;
-
-  while (RS485.available() > 0) {
-    char receivedVal = RS485.read();
+  while (Serial3.available() > 0) {
+    char receivedVal = Serial3.read();
 
     if (read == 1) {
 
       if (receivedVal == '>') {
-        Serial.print(message);
-        Serial.print("    ");
 
         for(int i = 0;i <= time;i++){
-          RS485.write(m[i]);
+          Serial3.write(m[i]);
           Serial.print(m[i]);
         }
+        
+        Serial.print("    ");
+        Serial.print(message);
 
         Serial.println();
         
