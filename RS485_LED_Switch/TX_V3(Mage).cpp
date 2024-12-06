6d1f96cce575ebeeb1cca05cb279c42222a0eeb5
m67yc <martinmcymay@gmail.com>
2024-12-06 15:15:59 +0800

    Update and rename TX_V3_Text.cpp to TX_V3(Mage).cpp

---
+++ RS485_LED_Switch/TX_V3(Mage).cpp
@@ -0,0 +1,60 @@
+#define buttonPin 13
+
+void setup() {
+  Serial.begin(9600);
+  Serial3.begin(9600);
+}
+
+long int success_count = 0;
+long int error_count = 0;
+int success_rate, count;
+char returnVal;
+char inputVal;
+unsigned long time, time2;
+
+void loop() {
+
+  if(count == 26)
+    count = 0;
+  inputVal = 'A' + count;
+  count++;
+  time = micros();
+  Serial3.write('<');
+  Serial3.write(inputVal);
+  Serial3.write('>');
+
+  while (!Serial3.available()) {
+  //   Serial.println("no");
+  //   if (inputVal == 'L') {
+  //     Serial3.write("<L>");
+  //   } else {
+  //     Serial3.write("<D>");
+  //   }
+  //   delay(10);
+  }
+  returnVal = Serial3.read();
+  time2 = micros();
+  Serial.print("    ");
+
+  if (returnVal != inputVal)
+    error_count++;
+  else
+    success_count++;
+  success_rate = success_count / (success_count + error_count) * 100;
+
+  unsigned long time_long = time2 - time;
+
+  Serial.print(returnVal);
+  Serial.print(" : ");
+  Serial.print(inputVal);
+  Serial.print("   ;   ");
+  Serial.print(error_count);
+  Serial.print("-");
+  Serial.print(success_count);
+  Serial.print(":  ");
+  Serial.print(success_rate);
+  Serial.print("%");
+  Serial.print("   ;   ");
+  Serial.print(time_long);
+  Serial.println("micros");
+}
